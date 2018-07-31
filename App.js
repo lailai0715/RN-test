365ed7a2c066b07fe70537d03e8bf91aa83d1fa9
lailai0715 <38026588+lailai0715@users.noreply.github.com>
2018-07-31 19:36:30 +0800

    Update App.js
    用 stylesheet 来修饰 CSS
    创造一个 StyleSheet 会更加容易了解 component 里的内容
    把 CSS 写进 StyleSheet 里

--- App.js
+++ App.js
@@ -32,33 +32,33 @@ import {Alert,Button, View, Image, Text, TextInput, WebView} from 'react-native'
 export default class ButtonTest extends Component{
     render(){
         return(
-            <View style={{flex:1,justifyContent:'center', alignItems:'center'}}>
-                <Button disabled={false}
+            <View style={styles.view}>
+                <Button style={{flex:1}}
+                        disabled={false}
                         onPress={onButtonPress}
                         title="buttonClick"
                     //  color:"#841584"
                         accessibilityLabel={"OK，Great!"}
-                        style={{flex:1}}
                 />
-                <Image
+                <Image style = {styles.image}
                     source={require('./ios/ic_logo.png')}
-                    style={{flex:1,width:50,height:50}}
                 />
-                <Image
+                <Image style = {styles.image}
                     defaultSource={require('./ios/ic_logo.png')}
                     source={{url:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530596559899&di=1cf6edc847e8a313aca72ae2780b314b&imgtype=0&src=http%3A%2F%2Fupload.qianlong.com%2F2016%2F1114%2F1479111325134.jpg'}}
-                    style={{flex:1,height:50}}
                 />
 
-                <Text style = {{flex:1,fontSize: 18, backgroundColor:"#2bb2c1",height:50}}>
-                        hello,World!
+                <Text style = {styles.text}>
+                     Hello World!
                 </Text>
 
-                <TextInput style={{flex:1,width:40,borderColor:"#2bb2c1", borderWidth:1}}
+                <TextInput style={styles.input}
+
                 />
-                <WebView style={{flex:1,backgroundColor:"red",width:100}}
-                         source={{url:'https://www.bejson.com/'}}
-                         scalesPageToFit={false}
+                  
+                <WebView style={styles.web}
+                     source={{url:'https://www.bejson.com/'}}
+                     scalesPageToFit={false}
                 />
             </View>
         );
@@ -68,3 +68,37 @@ export default class ButtonTest extends Component{
 const onButtonPress=()=>{
   Alert.alert('Button has been pressed!');
 };
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  
+  image: {
+    flex: 1,
+    width: 50,
+    height: 50,
+  },
+  
+  web: {
+    flex:1,
+    backgroundColor: 'red',
+    width: 100,
+  },
+  
+  input: {
+    flex:1,
+    width:40,
+    borderColor:"#2bb2c1", 
+    borderWidth:1
+  },
+ 
+  text: {
+    flex:1,
+    fontSize: 18, 
+    backgroundColor:"#2bb2c1",
+    height:50
+  },
+});
